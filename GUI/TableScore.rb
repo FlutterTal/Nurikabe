c59a0f67cc35b35e578573ce22511ffe3700a20c
FlutterTal <talunion72@gmail.com>
2020-02-19 14:53:55 +0100

    Création tableau de score plus fonctionnel

--- GUI/TableScore.rb
+++ GUI/TableScore.rb
@@ -3,10 +3,10 @@ require 'gtk3'
 module Gui
 
     class TableScore < Gtk::Table
-        def initialize
-            super(6, 2, true)
-            attach("Utilisateur", 0, 1, 0, 1)
-            attach("Temps", 1, 2, 0, 1)
+        def TableScore.creer
+            table = new(6, 2, true)
+            table.attach("Utilisateur", 0, 1, 0, 1)
+            table.attach("Temps", 1, 2, 0, 1)
         end
     end
     
