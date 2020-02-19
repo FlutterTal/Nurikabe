1f9729367d18b4f6c75b145c81106c1868154f1e
Vaidie Florent <s172581@ic2s112-05.univ-lemans.fr>
2020-02-19 15:37:24 +0100

    Doc + replay

--- Historique/HistoriqueElement.rb
+++ Historique/HistoriqueElement.rb
@@ -1,17 +1,29 @@
 require_relative '../Erreur/Erreur.rb'
 
+##
+# Un élément de l'historique
+#
+# Il représente le changement d'état d'une case ainsi que la validité du nouvel état
+# Tous ses attributs sont en lecture seule
 class HistoriqueElement
     attr_reader :case, :etat_avant, :etat_apres, :erreur
     private_class_method :new
 
+    ##
+    # Crée un élément de l'historique
+    #
+    # Prend en paramètres une case, son ancien état et son nouvel état
     def HistoriqueElement.Creer(case, etat_avant, etat_apres)
         new(case, etat_avant, etat_apres)
     end
 
+    ##
+    # Initialise l'élément et vérifie la validité du nouvel état
     def initialize(case, etat_avant, etat_apres)
         @case = case
         @etat_avant = etat_avant
         @etat_apres = etat_apres
+        # METTRE A JOUR PLUS TARD
         @erreur = Erreur.verif_case(case)
     end
 
