ca4b84a0cdc0771e7d7e937c9a6290ab16baaf94
gamheror <36196058+gamheror@users.noreply.github.com>
2020-02-19 18:21:48 +0100

    changement grille, case et utilisateur

--- Utilisateur/Utilisateur.rb
+++ Utilisateur/Utilisateur.rb
@@ -1,25 +1,52 @@
 class Utilisateur
-  attr_reader :nom, :credit, :progression, :niveau, :niveauTermine ,:temps, :fichier
   private_class_method :new
-
-  def Utilisateur.creer()
-    new(nom,credit,fichier)
+  attr_reader :nom ,:credit
+ 
+  def Utilisateur.creer(credit)
+    new(credit)
   end
 
-
-  def initialize(nom,credit,fichier)
+  def initialize(credit)
     puts("Saisir votre pseudo")
     @nom = gets
     @credit = 0
     @fichier = 'BDD_User.txt'
-
-    fichierUtilisateur = File.new(fichier,'w+')
-    fichierUtilisateur.write(@nom+";"+@credit)
+    fichierUtilisateur = File.open('BDD_User.txt','a+')
+    fichierUtilisateur.puts("#{@nom}#{@credit}")
     fichierUtilisateur.close
   end
 
-  def supprimer()
+  def supprimerUtilisateur()
+    tabUser = File.open('BDD_User.txt', 'r').readlines
+    fichierUtilisateur = File.open('BDD_User.txt','w')
+    i = 0
+    while i<= tabUser.size
+      if("#{@nom}"==tabUser[i])
+        i=i+2
+      end
+      fichierUtilisateur.puts(tabUser[i],tabUser[i+1])
+      i = i+2
+    end
+    fichierUtilisateur.close
+  end
 
+  #Permet de charger le nom d'un utilisateur ayant déjà été créé
+  def charger(nom)
+    tabUser = File.open('BDD_User.txt', 'r').readlines
+    fichierUtilisateur = File.open("BDD_User.txt", "r")
+    i = 0
+    if (tabUser.size==0)
+      return -1
+    else
+      while i<= tabUser.size
+        if(nom==tabUser[i])
+          return 1
+        end
+        i = i+2
+      end
+    end
+    fichierUtilisateur.close
+    return 0
   end
 
   def tempsMoyen()
@@ -31,6 +58,10 @@ class Utilisateur
       end
     end
     return @tempsMoyen/@niveauTermine
+  end
+
+  def nombreGrilles()
+
   end
 
   def to_s()
