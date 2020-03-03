f63527c490ff194f52213ee7106f1230dcfbe58c
Vaidie Florent <s172581@ic2s110-06.univ-lemans.fr>
2020-03-03 16:24:20 +0100

    update doc

--- Historique/HistoriqueElement.rb
+++ Historique/HistoriqueElement.rb
@@ -1,30 +1,32 @@
-require_relative '../Erreur/Erreur.rb'
+require_relative '../Grille/GrilleJouable.rb'
 
-##
-# Un élément de l'historique
-#
-# Il représente le changement d'état d'une case ainsi que la validité du nouvel état
-# Tous ses attributs sont en lecture seule
-class HistoriqueElement
-    attr_reader :case, :etat_avant, :etat_apres, :erreur
-    private_class_method :new
-
+module Historique
     ##
-    # Crée un élément de l'historique
+    # Un élément de l'historique
     #
-    # Prend en paramètres une case, son ancien état et son nouvel état
-    def HistoriqueElement.Creer(case, etat_avant, etat_apres)
-        new(case, etat_avant, etat_apres)
+    # Il représente le changement d'état d'une case ainsi que la validité du nouvel état
+    # Tous ses attributs sont en lecture seule
+    class HistoriqueElement
+        attr_reader :cases, :etat_avant, :etat_apres, :erreur
+        private_class_method :new
+    
+        ##
+        # Crée un élément de l'historique
+        #
+        # Prend en paramètres une case, son ancien état et son nouvel état
+        def HistoriqueElement.Creer(cases, etat_avant, etat_apres)
+            new(cases, etat_avant, etat_apres)
+        end
+    
+        ##
+        # Initialise l'élément et vérifie la validité du nouvel état
+        def initialize(cases, etat_avant, etat_apres)
+            @cases = cases
+            @etat_avant = etat_avant
+            @etat_apres = etat_apres
+            # METTRE A JOUR PLUS TARD
+            @erreur = (cases)
+        end
+    
     end
-
-    ##
-    # Initialise l'élément et vérifie la validité du nouvel état
-    def initialize(case, etat_avant, etat_apres)
-        @case = case
-        @etat_avant = etat_avant
-        @etat_apres = etat_apres
-        # METTRE A JOUR PLUS TARD
-        @erreur = Erreur.verif_case(case)
-    end
-
 end
