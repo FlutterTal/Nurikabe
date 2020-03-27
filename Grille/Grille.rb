887d71b7e89695351287f11bf811156c132b240e
gamheror <36196058+gamheror@users.noreply.github.com>
2020-03-27 15:16:47 +0100

    Commentaire

--- Grille/Grille.rb
+++ Grille/Grille.rb
@@ -1,7 +1,18 @@
-class Grille
-    attr_accessor :grille
+module Grille
 
-    def initialize()
-        @grille = Array.new()
+    ##
+    # La classe grille permet la création de GrilleJouable et GrilleStatique
+    class Grille
+
+        ##
+        # Variables d'instances
+        #   @grille
+
+
+        attr_accessor :grille
+
+        def initialize()
+            @grille = Array.new()
+        end
     end
 end
