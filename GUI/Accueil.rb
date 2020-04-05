ff2b7705984a671ee7b8721b1f984b07247122f5
Anthony Amiard <anthony.amiard@outlook.fr>
2020-04-05 17:59:42 +0200

    accueil gtk box

--- GUI/Accueil.rb
+++ GUI/Accueil.rb
@@ -4,58 +4,54 @@ module Gui
 
     ##
     # Widget graphique représentant l'écran d'accueil.
-    class Accueil < Gtk::Window
+    class Accueil < Gtk::Box
         
         ##
         # Méthode permettant de créer l'écran d'accueil.
-        def Accueil.creer
-            fenetre = new
-            fenetre.set_title("Nurikabe")
-            box = Gtk::Box.new(:vertical, 10)
-            fenetre.add(box)
-            box.show
+        def initialize
+            super(:vertical, 10)
 
-            tutorial = Gtk::Button.new
-            ltutorial = Gtk::Label.new
-            ltutorial.set_markup("<b>Tutorial</b>")
-            tutorial.add(ltutorial)
-            ltutorial.show
-            tutorial.signal_connect("clicked") {puts "Accès au tutorial"}
-            box.pack_start(tutorial, :expand => true, :fill => true, :padding => 0)
-            tutorial.show
+            self.pack_start(Gtk::Button.new.yield_self { |bouton|
+                bouton.add(Gtk::Label.new.yield_self { |label|
+                    label.set_markup("<b>Tutoriel</b>")
+                    label.show
+                    label
+                })
+                bouton.show
+                bouton
+            }, :expand => true, :fill => true, :padding => 0)
 
-            aventure = Gtk::Button.new
-            laventure = Gtk::Label.new
-            laventure.set_markup("<b>Aventure</b>\n" +
-                                "<span weight=\"light\" style=\"italic\">Niveau #number\tCrédits : 5</span>")
-            aventure.add(laventure)
-            laventure.show
-            aventure.signal_connect("clicked") {puts "Accès à l'aventure"}
-            box.pack_start(aventure, :expand => true, :fill => true, :padding => 0)
-            aventure.show
+            self.pack_start(Gtk::Button.new.yield_self { |bouton|
+                bouton.add(Gtk::Label.new.yield_self { |label|
+                    label.set_markup("<b>Aventure</b>")
+                    label.show
+                    label
+                })
+                bouton.show
+                bouton
+            }, :expand => true, :fill => true, :padding => 0)
 
-            arcade = Gtk::Button.new
-            larcade = Gtk::Label.new
-            larcade.set_markup("<b>Arcade</b>\n" +
-                              "<span weight=\"light\" style=\"italic\">#number terminés\tMoyenne : #temps</span>")
-            arcade.add(larcade)
-            larcade.show
-            arcade.signal_connect("clicked") {puts "Accès au mode arcade"}
-            box.pack_start(arcade, :expand => true, :fill => true, :padding => 0)
-            arcade.show
+            self.pack_start(Gtk::Button.new.yield_self { |bouton|
+                bouton.add(Gtk::Label.new.yield_self { |label|
+                    label.set_markup("<b>Arcade</b>")
+                    label.show
+                    label
+                })
+                bouton.show
+                bouton
+            }, :expand => true, :fill => true, :padding => 0)
 
-            options = Gtk::Button.new
-            loptions = Gtk::Label.new
-            loptions.set_markup("<b>Options</b>")
-            options.add(loptions)
-            loptions.show
-            options.signal_connect("clicked") {puts "Accès aux options"}
-            box.pack_start(options, :expand => true, :fill => true, :padding => 0)
-            options.show
-
-            fenetre.show
-
-            return fenetre
+            self.pack_start(Gtk::Button.new.yield_self { |bouton|
+                bouton.add(Gtk::Label.new.yield_self { |label|
+                    label.set_markup("<b>Options</b>")
+                    label.show
+                    label
+                })
+                bouton.show
+                bouton
+            }, :expand => true, :fill => true, :padding => 0)
+            
+            self.show
         end
     end
 
